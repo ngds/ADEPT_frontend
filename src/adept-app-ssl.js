3bb5f3b4b21f31c8c9877c221f0b36f0158f7091
Gary Hudman <gary.hudman.azgs@gmail.com>
2021-05-26 13:46:42 -0700

    adept standalone and docker

--- src/adept-app-ssl.js
+++ src/adept-app-ssl.js
@@ -1,33 +1,35 @@
-/* Adept Front End  App V1.2
+/* NGDS Adept Server
    G. Hudman
+  
+  Path - to local node application
+  certname - root name of cert fileSize
+  This configuration assumes the application will have certs located in /ssl directory under path
+  The local host IP given below is a default docker instance IP. 
+*/
 
-*/  
+var  Path = '/<WORKING-DIR>';
+var certname = '<WORKING-CERT>';
+var localHostIp = '172.17.0.2';
 
-require('dotenv').config();
-var  Path = process.env.NODE_PATH;
+var  fs = require("fs");
+console.log('Node Adept SSL app startup at ' + Date.now());
+
 const port = 443;
-const hostIP = '';
 var  fs = require("fs");
 
 var https = require("https");
-const cert = fs.readFileSync('../ssl/__XXX_org_cert.cer');
-const ca = fs.readFileSync('../ssl/__XXXX_org.cer');
-const key = fs.readFileSync('../ssl/MYSERVER.key');
+const cert = fs.readFileSync(Path+'/ssl/'+certname+'_cert.cer');
+const ca = fs.readFileSync(Path+'/ssl/'+certname+'.cer');
+const key = fs.readFileSync(Path+'/ssl/server.key');
 
 var credentials = { key: key,
                     cert: cert };
-
-const pg = require('pg');
-
-const connectionString = 'postgres://USER:PW@localhost:5432/DBNAME';
-
+                    
 var express = require('express'),
     app = express(),
-    request = require('request'),
-    bodyParser = require('body-parser');
+    request = require('request');
 
 var adeptapi =  require("./adept-routes.js");
-                   
 var httpsServer = https.createServer(credentials, app);
 
 app.use(function(req, res, next){
@@ -37,97 +39,24 @@ app.use(function(req, res, next){
     next();
 });
  
+
 app.use('/img', express.static(__dirname + '/public/img'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/css', express.static(__dirname + '/public/css'));
-
 app.use('/adept',adeptapi);
-const client = new pg.Client(connectionString);
-
-const results = [];
-client.connect();
-
-function routelog(req,lp, ore) {
-	var ip = req.headers['x-forwarded-for'] || 
-	req.connection.remoteAddress || 
-	req.socket.remoteAddress ||
-	(req.connection.socket ? req.connection.socket.remoteAddress : null);
-
-	if (ip.substr(0, 7) == "::ffff:") {
-	   ip = ip.substr(7)
-	}
-	var qs = '';
-	if ( req.query ) {
-		qs = '?';
-		for (var key in req.query) {
-			qs = qs + '&' + key + '=' + req.query[key];
-		  }
-	}
-	
-	var rm = req.method;
-	var nw = rd();
-
-	if ( typeof(ore) !== 'undefined' ) {
-		console.error(ip + ' ' + nw + ' ' + rm  + ' ' + lp + ' ' + qs + ' : ' + ore);
-	} else {
-		console.log(ip + ' ' + nw + ' ' + rm  + ' ' + lp + qs);
-	}		
-
-}
-
-function rd() {
-	var d = new Date();
-	d = '['+d.getFullYear() + "-" + ('0' + (d.getMonth() + 1)).slice(-2) 
-		+ "-" + ('0' + d.getDate()).slice(-2) + " " + ('0' + d.getHours()).slice(-2) 
-		+ ":" + ('0' + d.getMinutes()).slice(-2) + ":" + ('0' 
-		+ d.getSeconds()).slice(-2).slice(-2)+'.'+d.getMilliseconds()+']';
-	return d;
-}
 
 app.get('/' , function(req,res) {
-	console.log('path '+Path);
-	
-	 var lp = '/public/adept-ssl.htm';
-	 routelog(req, lp);
+	 var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+	 console.log('Remote IP ' + ip + ' ' + Path );
 	 res.header('Access-Control-Allow-Origin', '*');
 	 res.header('Access-Control-Allow-Headers', '*');
 	 res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-     res.sendFile(Path+lp);
+     res.sendFile(Path+'/public/adept-ssl.htm');
 } );
 
+httpsServer.listen(port,localHostIp)
 
-app.get('/adept' , function(req,res) {
-	var lp = '/public/adept-ssl.htm';
-	routelog(req, lp);
-	res.sendFile(Path+lp);
-} );
-
-
-app.get('/admin' , function(req,res) {
-	 
-	var lp = '/public/adept-admin.htm';
-	routelog(req, lp);
-	res.sendFile(Path+lp);
-
-} );
-
-app.get('/url_status', function(req, res) {
-	var lp = '/url_status';
-	routelog(req, lp);
-  	var exists = true;
-	res.send(exists);
-	
-	var urlToCheck = req.query.url;
-	console.log(' URL - ' + urlToCheck);
-	res.send(exists);
-	
-});	
-
-httpsServer.listen(port,localhost)
-
-/*
-app.listen(port, () => {
-  //console.log('App running on port ${port}.')
-  console.log('Startup  ' + rd() + ' ' + Path + ':' + port);
-});
-*/
